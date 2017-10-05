76ec3034ecd713428ddd81b01058ff44a4938813
Gergö Takács <gtakacs@thoughtworks.com>
2017-10-05 14:41:18 +0100

    fix example config

--- src/config.example.js
+++ src/config.example.js
@@ -1,5 +1,16 @@
 export default {
   clientId: '',
   scope: 'https://www.googleapis.com/auth/spreadsheets',
-  spreadsheetId: ''
+  spreadsheetId: '',
+  speedLowerBound: 25,
+  speedUpperBound: 35,
+  gridSize: 12,
+  timePerWord: 0.1,
+  range: 'A2:E',
+  mapping: {
+    name: 2,
+    from: 4,
+    message: 3,
+    secret: 4
+  }
 }
