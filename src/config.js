704cf4c949abc2843c20f9301f543495870a4bda
Mircea M <mircea.moise@gmail.com>
2020-03-04 16:09:36 +0000

    used the actual spreadsheet (not a copy) + add deployment to github pages

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-    apiKey: 'AIzaSyC4UvL_Yjq8Tef22E7Rj-HDafrxzRY4Iis', //google client ID
+    apiKey: 'AIzaSyC4UvL_Yjq8Tef22E7Rj-HDafrxzRY4Iis', //google api key
     clientId: '302154217901-42dk7gumote0945geu0bq6vpotaa4qqu.apps.googleusercontent.com', //google client ID
     scope: 'https://www.googleapis.com/auth/spreadsheets',
-    spreadsheetId: '1tzG2gBFiE75bDN7QctnlwpEbncR5u425LUFESLRZRFE', //ID for the spreadsheet
+    spreadsheetId: '17lpXtlODcFYHK5kdDSpuO0sXimVwUrzQMbEnMVj8h0c', //ID for the spreadsheet
     speedLowerBound: 25, //lower bound for flip randomisation
     speedUpperBound: 35, //upper bound for flip randomisation
     gridSize: 12, //grid size for the cards
@@ -10,9 +10,9 @@ export default {
     range: 'A2:E', //range for the data columns in the spreadsheet
     mapping: {
         //data mapping columns from spreadsheet
-        name: 2,
-        from: 1,
-        message: 3,
-        secret: 4
+        name: 3,
+        from: 2,
+        message: 4,
+        secret: 5
     }
 };
