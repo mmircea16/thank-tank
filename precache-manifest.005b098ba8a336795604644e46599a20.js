45d019c8829eb9a3e80d36291994a5c51b636415
Mircea M <mircea.moise@gmail.com>
2020-03-04 15:24:05 +0000

    Updates

---
+++ precache-manifest.005b098ba8a336795604644e46599a20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8178f6a44038742ecde7ff65417a797",
+    "url": "/thank-tank/index.html"
+  },
+  {
+    "revision": "8632042ba337d27d14ee",
+    "url": "/thank-tank/static/css/main.8f220cc4.chunk.css"
+  },
+  {
+    "revision": "221c05ac2becfc412fd8",
+    "url": "/thank-tank/static/js/2.8167a10b.chunk.js"
+  },
+  {
+    "revision": "c6821d28bfeabe6e24cca11b3bdc2152",
+    "url": "/thank-tank/static/js/2.8167a10b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8632042ba337d27d14ee",
+    "url": "/thank-tank/static/js/main.5dd7ba95.chunk.js"
+  },
+  {
+    "revision": "972a7833f0cbc96b0ac3",
+    "url": "/thank-tank/static/js/runtime-main.a3952116.js"
+  },
+  {
+    "revision": "76e0630202b039b031b818b0f0374249",
+    "url": "/thank-tank/static/media/logo-thanktank.76e06302.jpg"
+  }
+]);
